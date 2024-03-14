dcb9d1c18287267fa91a0efc64a472ec82eb5099
Abdul Hadi Khan Khokhar <112166841+A-Hadi-Kk@users.noreply.github.com>
2024-03-15 02:21:26 +0500

    Add files via upload

---
+++ ans26.ts
@@ -0,0 +1,17 @@
+//This program runs if block bcz alien_color is equals to green
+//let alien_color: string = 'green';
+//if (alien_color == 'green') {
+  //  console.log("Congratulations! You just earned 5 points.");
+//}
+//else{
+  //  console.log("Congratulations! You just earned 10 points.");
+//}
+
+//This program runs else block bcz alien_color is not equals to green
+let alien_color: string = 'yellow';
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+else{
+    console.log("Congratulations! You just earned 10 points.");
+}
---
+++ ans27.ts
@@ -0,0 +1,35 @@
+//This program prints while alien_color is equals to green
+//let alien_color: string = 'green';
+//if (alien_color == 'green') {
+  //  console.log("Congratulations! You just earned 5 points.");
+//}
+//else if (alien_color == `yellow`){
+  //  console.log("Congratulations! You just earned 10 points");
+//}
+//else if (alien_color == `red`){
+  //  console.log("Congratulations! You just earned 15 points");
+//}
+
+//This program prints while alien_color is equals to red
+//let alien_color: string = 'red';
+//if (alien_color == 'green') {
+  //  console.log("Congratulations! You just earned 5 points.");
+//}
+//else if (alien_color == `yellow`){
+  //  console.log("Congratulations! You just earned 10 points");
+//}
+//else if (alien_color == `red`){
+  //  console.log("Congratulations! You just earned 15 points");
+//}
+
+//This program prints while alien_color is equals to yellow
+let alien_color: string = 'yellow';
+if (alien_color == 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+else if (alien_color == `yellow`){
+    console.log("Congratulations! You just earned 10 points");
+}
+else if (alien_color == `red`){
+    console.log("Congratulations! You just earned 15 points");
+}
