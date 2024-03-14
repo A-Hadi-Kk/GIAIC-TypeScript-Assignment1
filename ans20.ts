400d4a8d3bdccf6c18128c02f0af1dcc89508ae7
Abdul Hadi Khan Khokhar <112166841+A-Hadi-Kk@users.noreply.github.com>
2024-03-14 14:50:32 +0500

    Add files via upload

---
+++ ans20.ts
@@ -0,0 +1,11 @@
+let languages: string[] = [
+    "English",
+    "Urdu",
+    "Spanish",
+    "French",
+    "Balochi",
+    "Siraiki",
+    "Sindhi"
+];
+console.log("List of some Languages: ");
+console.log(languages);
---
+++ ans21.ts
@@ -0,0 +1,12 @@
+let cars: {[key: string]: {name: string, model: number, owname: string}}={
+    "Toyota": { name:"Altis", model:2017, owname:"Hanzala" },
+    "Audi": {name:"A8", model:2024, owname:"Aashir"},
+    "Nissan": {name:"Gtr", model:2004, owname:"Asim"},
+};
+console.log("Info about my frnds cars");
+for (let car in cars){
+    console.log(`Car: ${car}`);
+    console.log(`Model: ${cars[car].model}`);
+    console.log(`Owner's Name: ${cars[car].owname}`);
+    console.log();
+}
